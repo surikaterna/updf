e91c39aa06edb838b84411a17d93e0b7286ba698
Andreas Karlsson <sprawl@onlajn.nu>
2017-03-15 22:39:18 +0100

    thinking of how to do layout in less code...

--- test/container.js
+++ test/container.js
@@ -6,21 +6,29 @@ function display(vdom) {
   return (vdom.style && vdom.style.display) || vdom.type;
 }
 
+function style(props, values) {
+  props.style = Object.assign(props.style || {}, values);
+  return props.style;
+}
+
 function layouter(vdom) {
   console.log('>', vdom.type, Object.keys(vdom), vdom.children);
-  vdom.props.style = vdom.props.style || {};
-  const dsp = display(vdom);
+  style(vdom.props); //.style = vdom.props.style || {};
   let x = 0;
   let y = 0;
+  let currentLineHeight = 0;
   if (vdom.children && vdom.children.length > 0) {
     vdom.children.forEach(ch => {
-      console.log('>', ch.props.style.width);
-      layouter(ch);
-      console.log('<', ch.props.style.width);
-      // consider position attribute
-      if (dsp === 'inline') {
-        x = Math.max(x, ch.props.style.width);
+      const dsp = display(ch);
+      if (dsp === 'block') {
+        x = 0;
+        y += currentLineHeight; // change to a new line
+      } else {
+        //currentLineHeight = Math.max(ch.props.height, currentLineHeight);
+        //ch.props.style
       }
+      layouter(ch);
+      // consider position attribute
       y += ch.props.style.height || 0;
       console.log(x, y);
     });
@@ -28,6 +36,8 @@ function layouter(vdom) {
     vdom.props.style.height = y;
   }
 };
+
+
 
 function dumpDom(vdom, indent = 0) {
   let out = '';
@@ -53,10 +63,12 @@ function dumpDom(vdom, indent = 0) {
 describe('container', () => {
   it.only('should limit size of children to container', () => {
     const b = block({},
-      block({ style: { width: 100 } },
+      block({ style: { top: 100, left: 40, width: 100 } },
         block({ style: { width: 120 } },
           inline({}, 'Hello World!')
-        )));
+        )
+      )
+    );
     layouter(b);
     dumpDom(b);
     b.props.style.width.should.equal(0);
