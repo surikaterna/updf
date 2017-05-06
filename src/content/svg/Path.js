b78d8c7f382b0728a77a24ce4bb11a416d152d34
Andreas Karlsson <sprawl@onlajn.nu>
2017-05-06 11:49:43 +0200

    path 2d mapping

--- src/content/svg/Path.js
+++ src/content/svg/Path.js
@@ -1,12 +1,20 @@
 import bind from '../bind';
 import pathParser from './pathParser';
 
+const mapping = {
+  M: 'moveTo',
+  L: 'lineTo',
+  m: 'moveToR',
+  l: 'lienToR'
+};
 
 const Path = (props) => {
   const vector = {};
+ 
   pathParser(props.d || '', {
     M: (x, y) => vector.moveTo(x, y),
-    L: (x, y) => vector.lineTo(x, y)
+    L: (x, y) => vector.lineTo(x, y),
+    m: (x, y) => vector.moveTo(x, y),
   });
 };
 
