cfec21d6fec532ef621895c4f8090e1784d4b96d
Andreas Karlsson <sprawl@onlajn.nu>
2017-05-09 13:54:53 +0200

    added inital css parsing

--- src/content/styles/cssParser.js
+++ src/content/styles/cssParser.js
@@ -1,6 +1,14 @@
-import { collect } from '../svg/pathParser';
-const re = /(([^{]+)\s*\{\s*([^}]+)\s*})/g;
+import asStyle from '../util/asStyle';
 
 export default function cssParser(css) {
-  console.log(collect(css, re));
+  const rules = [];
+  const parts = css.trim().split(/[{}]/).filter(String).map((str) => str.trim());
+  while (parts.length > 0) {
+    const ruleData = parts.splice(0, 2);
+    // console.log('RDD', ruleData);
+    const style = asStyle(ruleData[1]);
+     console.log('RD', ruleData[0], style);
+    rules.push({ sel: ruleData[0], style });
+  }
+  return rules; // console.log('C1', parts);
 }
--- test/content/styles/cssParser.js
+++ test/content/styles/cssParser.js
@@ -8,8 +8,13 @@ describe('cssParser', () => {
   describe('#parse', () => {
     it.only('should parse one class descriptor', () => {
       cssParser(`
-        .className {style: 'value'}
-        `).rules.length.should.equal(1);
+        .className {style: 'value'; style2: 'v2'}
+        .className {style: 'value2'}
+        `).length.should.equal(2);
+        console.log(cssParser(`
+        .className {style: 'value'; style2: 'v2'}
+        .className {style: 'value2'}
+        `))
     });
   });
 });
