e2aa159fc385883dfd7a05fc7f65cbda7678e650
pgrobban <robert.sebescen@surikat.se>
2021-10-12 09:57:40 +0200

    XL-3787 handle replaceDiacritics with special case such as polish Ł

--- src/content/util/replaceDiacritics.js
+++ src/content/util/replaceDiacritics.js
@@ -1,10 +1,16 @@
 const unicodeChars = /[\u0300-\u036F]/g;
+const specialCharsTranslations = {
+  Ł: 'L', ł: 'l'
+};
+const specialChars = Object.keys(specialCharsTranslations);
+const specialCharsRegexp = new RegExp(`[${specialChars.join('')}]`, 'g');
 
 export default function replaceDiacritics(str) {
   if (typeof str !== 'string') {
     return str;
   }
-  const normalized = str.normalize('NFKD').replace(unicodeChars, '');
+
+  const normalized = str.normalize('NFKD').replace(specialCharsRegexp, $0 => specialCharsTranslations[$0]).replace(unicodeChars, '');
   const buffer = Buffer.from(normalized, 'utf8');
   return buffer.toString('ascii');
 }
